76c29273e8e07f88f687f8e19497ee062b884a29
matilde-amorim <120378461+matilde-amorim@users.noreply.github.com>
2025-04-19 17:15:35 +0100

    Add files via upload

---
+++ Front-office/FO-Report.js
@@ -0,0 +1,12 @@
+function openMenu() {
+    document.getElementById("sideMenu").classList.add("open");
+  }
+  
+  function closeMenu() {
+    document.getElementById("sideMenu").classList.remove("open");
+  }
+  
+  function handleReport() {
+    alert("Report de ocorrência iniciado!");
+  }
+  
