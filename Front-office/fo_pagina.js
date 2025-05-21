276fe3a33702b94e40154e36bcb7f976dcf514da
matiiii333 <a104148@uminho.pt>
2025-05-21 13:57:37 +0100

    atualizar

---
+++ Front-office/fo_pagina.js
@@ -0,0 +1,33 @@
+// Função para abrir o menu lateral
+function openMenu() {
+    document.getElementById("sideMenu").classList.add("open");
+  }
+  
+  // Função para fechar o menu lateral
+  function closeMenu() {
+    document.getElementById("sideMenu").classList.remove("open");
+  }
+  
+  // Função para redirecionar ao clicar no cartão
+function navigateToPage(page) {
+  window.location.href = page;  // Redireciona para a página especificada
+}
+
+// Adicionar ouvintes de evento para cada cartão
+document.getElementById('report-card').addEventListener('click', function() {
+  navigateToPage('report.html'); // Redireciona para a página de Report
+});
+
+
+document.getElementById('auditorias-card').addEventListener('click', function() {
+  navigateToPage('auditorias.html'); // Redireciona para a página de Auditorias
+});
+
+document.getElementById('registos-card').addEventListener('click', function() {
+  navigateToPage('registos.html'); // Redireciona para a página de Registros
+});
+
+window.onload = function() {
+  const image = document.querySelector('.animated-image');
+  image.classList.add('animate');
+};
